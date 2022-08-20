7059f4f180fe3fc913509fe117678190d0f151ac
Ian H <ianlouishawthorne@gmail.com>
2022-08-19 22:01:25 -0500

    added tictactoe semi functionality

--- web/static/js/tictac.js
+++ web/static/js/tictac.js
@@ -1,11 +1,15 @@
 const board = {1:"n",2:"n",3:"n",4:"n",5:"n",6:"n",7:"n",8:"n",9:"n"};
 let going = "x";
-function clicked(id) {
-    let idd = parseInt(id);
+function clicked(obj) {
+    console.log(board);
+    let idd = parseInt(obj.id);
     if (board[idd] == "n") {
         board[idd] = going;
+        obj.src="/static/imgs/tictactoe/" + going.toUpperCase() + ".png"
     }
     if (going == "x") {
         going = "o"
+    } else {
+        going = "x";
     }
 }
